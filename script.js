260a3d5c99680d360d1555a760dcad6a973df2d1
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-01-21 21:35:03 +0500

    nasa opod

---
+++ script.js
@@ -0,0 +1,15 @@
+const resultsNav = document.getElementById('resultsNav');
+const favoritesNav = document.getElementById('favoritesNav');
+const imagesContainer = document.querySelector('.images-container');
+const saveConfirmed = document.querySelector('.save-confirmed');
+const loader = document.querySelector('.loader');
+
+// NASA API
+const count = 10;
+const apiKey = 'DEMO_KEY';
+const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
+
+let resultsArray = [];
+let favorites = {};
+
+// 
