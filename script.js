e98f6d028e3eb7132bf8abcd0a1096c0860edbf8
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-01-22 16:54:36 +0500

    fetch api

--- script.js
+++ script.js
@@ -12,4 +12,66 @@ const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${co
 let resultsArray = [];
 let favorites = {};
 
-// 
+function updateDOM() {
+  resultsArray.forEach((result) => {
+    // Card container
+    const card = document.createElement('div');
+    card.classList.add('card');
+    // link
+    const link = document.createElement('a');
+    link.href = result.hdurl;
+    link.title = 'View Full Image';
+    link.target = '_blank';
+    // Image
+    const image = document.createElement('img');
+    image.src = result.url;
+    image.alt = 'NASA Picture of the day';
+    image.loading = 'lazy';
+    image.classList.add('card-img-top');
+    // Card body
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+    // Card Title
+    const cardTitle = document.createElement('h5');
+    cardTitle.classList.add('card-title');
+    cardTitle.textContent = result.title;
+    // Save Text
+    const saveText = document.createElement('p');
+    saveText.classList.add('clickable');
+    saveText.textContent = 'Add To Favorites';
+    // Card Text
+    const cardText = document.createElement('p');
+    cardText.textContent = result.explanation;
+    // Footer Container
+    const footer = document.createElement('small');
+    footer.classList.add('text-muted');
+    // Date
+    const date = document.createElement('strong');
+    date.textContent = result.date;
+    // Copyright
+    const copyrightResult = result.copyright === undefined ? '' : result.copyright
+    const copyright = document.createElement('span');
+    copyright.textContent = ` ${copyrightResult}`;
+    // Append
+    footer.append(date, copyright)
+    cardBody.append(cardTitle, cardText, saveText, footer);
+    link.appendChild(image)
+    card.append(link, cardBody)
+    imagesContainer.appendChild(card)
+    console.log(card);
+  })
+}
+
+// Get 10 images from nasa api
+async function getNasaPictures() {
+  try {
+    const response = await fetch(apiUrl);
+    resultsArray = await response.json()
+    console.log(resultsArray);
+    updateDOM()
+  } catch (error) {
+    
+  }
+}
+
+getNasaPictures()
